075d9f88f81dd8c33e225096ad2adbcb03195c7f
DipPandey <83689392+DipPandey@users.noreply.github.com>
2024-03-19 11:11:22 +1100

    port changed

--- Helalth Appointment Scheduler/server.js
+++ Helalth Appointment Scheduler/server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3000;
 
 // Set up middleware to serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
