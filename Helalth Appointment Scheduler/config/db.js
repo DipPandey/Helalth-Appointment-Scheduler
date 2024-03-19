ea611cfbc2ccc2ec932335d5a77010f228f2fc9b
DipPandey <83689392+DipPandey@users.noreply.github.com>
2024-03-19 12:51:54 +1100

    Mongo added

---
+++ Helalth Appointment Scheduler/config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+async function connectDB() {
+    try {
+        await mongoose.connect('mongodb://localhost/HAS', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log('Connected to MongoDB');
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error);
+        process.exit(1); // Exit process with failure
+    }
+}
+
+module.exports = connectDB;
