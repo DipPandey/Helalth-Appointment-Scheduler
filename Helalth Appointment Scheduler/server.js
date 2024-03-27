c5f98e768c08c3837e5c731c01ca3d0a303aa3c1
DipPandey <83689392+DipPandey@users.noreply.github.com>
2024-03-27 18:14:19 +1100

    server updated

--- Helalth Appointment Scheduler/server.js
+++ Helalth Appointment Scheduler/server.js
@@ -1,35 +1,36 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
-const authRoutes = require('./routes/authRoutes.js'); // Adjust the path to where your authRoutes.js file is located.
-
-
-
-app.use(express.json()); // for parsing application/json
-app.use('/auth', authRoutes); // Set up routes
-
-// Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/HAS', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-  .then(() => console.log('Successfully connected to HAS MongoDB.'))
-  .catch((error) => console.error('MongoDB connection error:', error));
 
 // Initialize Express
 const app = express();
 
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/HAS', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .then(() => console.log('Successfully connected to HAS MongoDB.'))
+    .catch((error) => console.error('MongoDB connection error:', error));
+
+// Middleware for parsing application/json
+app.use(express.json());
+
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Define authRoutes after the express app is initialized and middleware set
+// Make sure this path is correct relative to your server.js file
+const authRoutes = require('./routes/authRoutes');
+app.use('/auth', authRoutes);
+
 // Route to serve the index.html file
 app.get('/', (req, res) => {
-  // Change the path to your actual path to the index.html if it's different
-  res.sendFile(path.join(__dirname, 'public'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html')); // Ensure this path points to your actual index.html file
 });
 
 // Start the server
 const PORT = process.env.PORT || 1337;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
