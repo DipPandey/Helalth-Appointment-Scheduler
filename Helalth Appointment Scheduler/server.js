a4bc741a182270c481d92a44346961abfcf0d147
DipPandey <83689392+DipPandey@users.noreply.github.com>
2024-03-27 18:09:12 +1100

    back to prev

--- Helalth Appointment Scheduler/server.js
+++ Helalth Appointment Scheduler/server.js
@@ -1,34 +1,35 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
+const authRoutes = require('./routes/authRoutes'); // Adjust the path to where your authRoutes.js file is located.
+
+
+
+app.use(express.json()); // for parsing application/json
+app.use('/auth', authRoutes); // Set up routes
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/HAS', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => console.log('Successfully connected to HAS MongoDB.'))
+  .catch((error) => console.error('MongoDB connection error:', error));
 
 // Initialize Express
 const app = express();
 
-// Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/HAS', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-    .then(() => console.log('Successfully connected to HAS MongoDB.'))
-    .catch((error) => console.error('MongoDB connection error:', error));
-
-app.use(express.json()); // for parsing application/json
-
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Define routes after the express app is initialized and middleware set
-const authRoutes = require('./routes/authRoutes'); // Make sure this path is correct
-app.use('/auth', authRoutes);
-
 // Route to serve the index.html file
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html')); // Ensure this path is correct
+  // Change the path to your actual path to the index.html if it's different
+  res.sendFile(path.join(__dirname, 'public'));
 });
 
 // Start the server
 const PORT = process.env.PORT || 1337;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
