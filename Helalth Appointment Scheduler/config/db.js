83e49e164cb868403c775db92bf3ade42ffab697
DipPandey <83689392+DipPandey@users.noreply.github.com>
2024-08-20 13:40:00 +1000

    push the changes

--- Helalth Appointment Scheduler/config/db.js
+++ Helalth Appointment Scheduler/config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI = 'mongodb://localhost:27017/HAS';
 const db = mongoose.connection;
 
 mongoose.connect(MONGODB_URI, {
